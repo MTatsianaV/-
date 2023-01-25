c2177e7ba1ac2df38d6769ad690e9a73c355af98
Marozava <mortatval@gmail.com>
2023-01-25 21:35:18 +0300

    created task14

---
+++ task14/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая:
+1. принимает на вход число
+2. проверяет, кратно ли оно одновременно 7 и 23.
+Например:
+14 -> нет
+46 -> нет
+161 -> да */
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int remainder = Remainder(number);
+if (remainder == 0) Console.WriteLine("да");
+else Console.WriteLine("нет");
+int Remainder (int num)
+{
+    int rem1 = num % 7;
+    int rem2 = num % 23;
+    int rem = rem1 + rem2;
+    return rem;
+}
