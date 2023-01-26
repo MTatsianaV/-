43ee011b2470074fcf3974a04711cd80334c395c
Marozava <mortatval@gmail.com>
2023-01-26 09:20:33 +0300

    created task10

---
+++ task10/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Напишите программу, которая:
+1. принимает на вход трёхзначное число
+2. на выходе показывает вторую цифру этого числа.
+Например:
+456-> 5
+782-> 8
+918-> 1 */
+int Result(int num)
+{
+    int num1 = (num - (num / 100) * 100) / 10;
+    return num1;
+}
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = number * -1;
+if (number > 99 && number < 1000)
+{
+int result = Result(number);
+Console.WriteLine($"Вторая цифра числа: {result}");
+}
+else Console.WriteLine("Вы ввели не трёхзначное число! ");
