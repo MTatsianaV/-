228ae4c11c4196ff34d7e7d4f5de5267998b1bdd
Marozava <mortatval@gmail.com>
2023-01-28 20:51:25 +0300

    created task28

---
+++ task28/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая:
+1. принимает на вход число N
+2. выдаёт произведение чисел от 1 до N.
+Например:
+4 -> 24
+5 -> 120 */
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Некорректный ввод! ");
+else 
+{
+    int productNumbers = ProductNumbers(number);
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {productNumbers}");
+}
+int ProductNumbers(int num)
+{
+    int product = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        product = product * i;
+    }
+    return product;
+}
