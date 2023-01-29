ee1bdedb69dd4fa93857727dfad1833e98dc1c4f
Marozava <mortatval@gmail.com>
2023-01-29 11:03:55 +0300

    created task27

---
+++ task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая:
+1. принимает на вход число
+2. выдаёт сумму цифр в числе.
+Например:
+452 -> 11
+82 -> 10
+9012 -> 12 */
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digitsSum = DigitsSum (number);
+Console.WriteLine($"Сумма цифр в числе {number} равна {digitsSum} ");
+int DigitsSum (int num)
+{
+if (num < 0) num = num * -1;
+int sum = 0;
+int dig = 0;
+while (num > 0)
+{
+   dig = num % 10;
+   sum = sum + dig;
+   num = num / 10; 
+}
+return sum;
+}
