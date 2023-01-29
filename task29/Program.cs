9ab1e377e0aa6cf40be8f4c2c9a5ecac2b368aa2
Marozava <mortatval@gmail.com>
2023-01-29 11:38:59 +0300

    created task29

---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая:
+1. задаёт массив из 8 элементов 
+2. заполненный псевдослучайными числами
+3. выводит их на экран.
+Например:
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+Random rnd = new Random();
+int[] GenerateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for (int i =0; i < arr.Length; i++)  
+    {
+      arr[i] = rnd.Next(min, max + 1);  
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+      Console.Write($"{arr[i]}");
+      if (i < arr.Length - 1) Console.Write($", ");
+    }
+}
+int[] array = GenerateArray(8, -99, 99);
+PrintArray(array); Console.Write(" -> ["); PrintArray(array); Console.Write("]");
